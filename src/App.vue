86fc4adbfa55e7f9774773bf22eab17977de655d
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2023-07-07 19:13:12 -0300

    se agrega router y ruta home

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,10 @@
 <template>
   <v-app class="font">
-    <KeepAlive>
-      <BtnLogout />
-    </KeepAlive>
-    <BtnMenu v-if="user.userLogget" />
-    <KeepAlive>
-      <BtnMusic v-if="user.userLogget" />
-    </KeepAlive>
-    <!-- <Loading v-if="user.loading" /> -->
-    <Loading v-if="user.loading && !user.userLogget" />
-    <Suspense>
-      <Login v-if="!user.userLogget" />
-      <v-main v-else>
-        <HeaderName />
-        <Paragraph text="La fiesta esta por empezar...." />
-        <Paragraph text="18 de Septiembre 2023" />
-        <Count />
-        <CardLocation />
-        <DressCode />
-        <CardTask />
-        <Confirm />
-        <Gift />
-        <Spotify />
-        <Footer />
-      </v-main>
-    </Suspense>
+    <router-view />
   </v-app>
 </template>
 
 <script setup>
-import CardLocation from './components/CardLocation.vue';
-import CardTask from './components/CardTask.vue';
-import Confirm from './components/Confirm.vue';
-import Count from './components/Count.vue';
-import DressCode from './components/DressCode.vue';
-import Gift from './components/Gift.vue';
-import HeaderName from './components/HeaderName.vue';
-import Mis15 from './components/Mis15.vue';
-import Paragraph from './components/Paragraph.vue';
-import Spotify from './components/Spotify.vue';
-import Footer from './components/Footer.vue'
-import BtnMusic from './components/BtnMusic.vue'
-import Login from './components/Login.vue';
-//Stores
-import { useAuthStore } from './stores/AuthStore';
-import Loading from './components/Loading.vue';
-import BtnLogout from './components/BtnLogout.vue';
-import BtnMenu from './components/BtnMenu.vue';
-
-
-const user = useAuthStore();
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Imperial+Script&family=Libre+Caslon+Display&display=swap');
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+    <KeepAlive>
+      <BtnLogout />
+    </KeepAlive>
+    <BtnMenu v-if="user.userLogget" />
+    <KeepAlive>
+      <BtnMusic v-if="user.userLogget" />
+    </KeepAlive>
+    <!-- <Loading v-if="user.loading" /> -->
+    <Loading v-if="user.loading && !user.userLogget" />
+    <Suspense>
+      <Login v-if="!user.userLogget" />
+      <v-main v-else>
+        <HeaderName />
+        <Paragraph text="La fiesta esta por empezar...." />
+        <Paragraph text="18 de Septiembre 2023" />
+        <Count />
+        <CardLocation />
+        <DressCode />
+        <CardTask />
+        <Confirm />
+        <Gift />
+        <Spotify />
+        <Footer />
+      </v-main>
+    </Suspense>
+</template>
+
+<script setup>
+import CardLocation from '@/components/CardLocation.vue';
+import CardTask from '@/components/CardTask.vue';
+import Confirm from '@/components/Confirm.vue';
+import Count from '@/components/Count.vue';
+import DressCode from '@/components/DressCode.vue';
+import Gift from '@/components/Gift.vue';
+import HeaderName from '@/components/HeaderName.vue';
+import Paragraph from '@/components/Paragraph.vue';
+import Spotify from '@/components/Spotify.vue';
+import Footer from '@/components/Footer.vue'
+import BtnMusic from '@/components/BtnMusic.vue'
+import Login from '@/components/Login.vue';
+import Loading from '@/components/Loading.vue';
+import BtnLogout from '@/components/BtnLogout.vue';
+import BtnMenu from '@/components/BtnMenu.vue';
+
+// STORES
+import { useAuthStore } from '@/stores/AuthStore';
+
+const user = useAuthStore();
+</script>
